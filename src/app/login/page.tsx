71138d54551e20fda42dca0e3a71a0c38e485f38
Kwesi Hervie <herviek2001@gmail.com>
2025-04-01 08:50:46 +0100

    ui fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,18 +6,11 @@ const LoginPage = () => {
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow">
-                <div className="text-center">
-                    <h1 className="text-2xl font-bold">Admin Login</h1>
-                    <p className="text-gray-600">Sign in to your admin account</p>
-                </div>
-
-                <Suspense fallback={<section className='w-full h-full min-h-screen flex items-center justify-center'>
-                    <p className='text-body'>Loading</p>
-                </section>}>
-                    <LoginForm />
-                </Suspense>
-            </div>
+            <Suspense fallback={<section className='w-full h-full min-h-screen flex items-center justify-center'>
+                <p className='text-body'>Loading</p>
+            </section>}>
+                <LoginForm />
+            </Suspense>
         </div>
     );
 };
